88d682c1fbb41e18dbe6236ada2f745446dffd95
hollyshlt <rflppanza@yahoo.com.br>
2023-12-05 13:48:58 -0300

    1.0

---
+++ src/Principal.java
@@ -0,0 +1,55 @@
+import br.com.alura.screenmatch.calculo.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculo.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Filme favorito = new Filme();
+
+        favorito.setNome("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
+
+        favorito.exibeFichaTecnica();
+        favorito.avalia(4);
+        favorito.avalia(5);
+        favorito.avalia(1);
+
+        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme outroFavorito = new Filme();
+
+        outroFavorito.setNome("The Matrix");
+        outroFavorito.setAnoDeLancamento(1999);
+        outroFavorito.setDuracaoEmMinutos(200);
+        outroFavorito.setIncluidoNoPlano(true);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(outroFavorito);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(favorito);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(500);
+        filtro.filtra(episodio);
+    }
+}
