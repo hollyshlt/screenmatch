6e349a56f96e83647170dc0846aac47a01599661
hollyshlt <rflppanza@yahoo.com.br>
2023-12-08 13:57:55 -0300

    1.4

--- src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
@@ -9,8 +9,11 @@ import br.com.alura.screenmatch.modelos.Titulo;
 public class PrincipalComListas {
     public static void main(String[] args) {
         Filme favorito = new Filme("The matrix", 1999);
+        favorito.avalia(9);
         Filme outroFavorito = new Filme("O poderoso chefão", 1972);
+        outroFavorito.avalia(10);
         var filmeDoRafael = new Filme("Dogville", 2003);
+        filmeDoRafael.avalia(9);
         Serie lost = new Serie("Lost", 2000);
 
         ArrayList<Titulo> lista = new ArrayList<>();
@@ -18,6 +21,13 @@ public class PrincipalComListas {
         lista.add(favorito);
         lista.add(outroFavorito);
         lista.add(lost);
+
+        for (Titulo item : lista) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() >2) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+        }
     }
     
 }
